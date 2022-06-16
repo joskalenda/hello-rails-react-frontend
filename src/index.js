327ae77bbd26c76c8aeed3a14e75ccb266d3a493
joskalenda <joskalenda3@gmail.com>
2022-06-16 18:29:25 +0200

    fix linters

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store}
       <Router>
         <App />
       </Router>
